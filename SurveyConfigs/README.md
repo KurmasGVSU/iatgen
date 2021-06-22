ff57354fa3a505cd32fdd9c6564d0a1e9cb1ddf2
Zachary Kurmas <kurmasz@gvsu.edu>
2021-06-22 11:08:10 -0400

    Added good/bad words.

---
+++ SurveyConfigs/README.md
@@ -0,0 +1,5 @@
+To execute a script:
+
+R < script_name.R --no-save
+
+(On my mac, I needed to alias R to /Library/Frameworks/R.framework//Versions/Current/Resources/bin/R)
---
+++ SurveyConfigs/soundsWords.R
@@ -0,0 +1,39 @@
+library(purrr)
+devtools::install("/Users/kurmasz/Documents/LocalResearch/IAT/MyFork/iatgen")
+library(iatgen)
+# devtools::load_all(iatgen)
+
+baseURL <- "https://www.cis.gvsu.edu/~kurmasz/IATsounds/"
+
+sounds <- c("cele", "cipo", "zapa", "zulo") 
+
+sSounds <- map(sounds, ~ paste(baseURL, .x, "_s.wav", sep=""))
+thSounds <- map(sounds, ~ paste(baseURL, .x, "_z.wav", sep=""))
+
+print("sSounds:  ")
+print(sSounds)
+print("thSounds:  ")
+print(thSounds)
+
+writeIATfull(IATname="sth_v1",
+             posname="Good", 
+             negname="Bad",
+             Aname="'s' sound", 
+             Bname="'th' sound",
+             catType="words",
+             poswords = c("Marvelous", "Superb", "Pleasure", "Beautiful", "Joyful", "Glorious", "Lovely", "Wonderful"),
+             negwords = c("Tragic", "Horrible", "Agony", "Painful", "Terrible", "Awful", "Humiliate", "Nasty"),
+             tgtType="images",
+             Aimgs = sSounds,
+             Bimgs = thSounds,
+             
+             #advanced options with recommended IAT settings
+             n=c(20, 20, 20, 40, 40, 20, 40),   # number of trials per block
+             qsf=TRUE,                          # outputs qualtrics file. If false, generates raw code
+             note=TRUE,                         # include note reminding online particpnats of key assignments
+             correct.error=TRUE,                # require particpnats to correct errors before proceeding
+             pause=250,                         # pause between trials
+             errorpause=300,                    # duration error message on screen; ignored if correct.error=TRUE
+             tgtCol="black",                    # color for target labels + text stimuli
+             catCol="green"                     # color for category labels + text stimuli
+)
